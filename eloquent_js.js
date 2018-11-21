c607431ed4b785084d5feac671e82e4f71e6702f
Sam <sam.ludwig@gmail.com>
2018-11-21 22:21:18 +1100

    first commit of README and JS

---
+++ eloquent_js.js
@@ -0,0 +1,8 @@
+//Challenge 1
+// Defining an array containing all 7 required cl  outputs.
+let triangle = ["#","##","###","####","#####","######","#######"]
+// Iterating through triangle array according to index and array.length.
+for (let i = 0; i < triangle.length; i++) {
+// console logging for each iteration until index is = array.length
+ console.log(triangle[i])
+}
