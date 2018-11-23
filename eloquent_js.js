1961fa573422b20522dfee1496cadc14b11309cb
Sam <sam.ludwig@gmail.com>
2018-11-23 22:51:57 +1100

    bean-counter

--- eloquent_js.js
+++ eloquent_js.js
@@ -1,118 +1,117 @@
 // Program Structure
 
-//Challenge 1
+// Challenge 1
 // Looping a triangle
 
-// // Setting empty triangle variable.
-// let triangle = ""
-// // while loop iterates while triangle length is less than 7.
-// // Each iteration while less than 7 adds 1 # to the triangle string
-// while(triangle.length < 7){ triangle = triangle +"#"
-// console.log(triangle)
-// }
+// Setting empty triangle variable.
+let triangle = ""
+// while loop iterates while triangle length is less than 7.
+// Each iteration while less than 7 adds 1 # to the triangle string
+while(triangle.length < 7){ triangle = triangle +"#"
+console.log(triangle)
+}
 
 // ______________________________________________________________________________
 
-//Challenge 2
+// Challenge 2
 // FIZZBUZZ
-//
-// //Defining my variables. Counter and number
-// let number = 0
-// let counter = 0
-// // While loop will iterate until counter is 100
-// while(counter < 100) {
-//   // Each interation will add 1 to both number and counter.
-//   number = number +1
-//   counter = counter +1
-//   // loop will return fizzbuzz if number is divislb by 3 and 5
-//   if (number % 3 == 0 && number % 5 == 0) { console.log("FIZZBUZZ")}
-//   // loop will return fizz when number is divisble by 3
-//   else if (number % 3 == 0) { console.log("FIZZ")}
-//   // loop wil return buzz when number is divisble by 5
-//   else if (number % 5 == 0) { console.log("BUZZ")}
-// // if number is not divisble by 3 or 5 it will be logged to screen.
-//   else {
-//     console.log(number)
-//   }
-// }
+
+//Defining my variables. Counter and number
+let number = 0
+let countar = 0
+// While loop will iterate until counter is 100
+while(countar < 100) {
+  // Each interation will add 1 to both number and counter.
+  number = number +1
+  countar = countar +1
+  // loop will return fizzbuzz if number is divislb by 3 and 5
+  if (number % 3 == 0 && number % 5 == 0) { console.log("FIZZBUZZ")}
+  // loop will return fizz when number is divisble by 3
+  else if (number % 3 == 0) { console.log("FIZZ")}
+  // loop wil return buzz when number is divisble by 5
+  else if (number % 5 == 0) { console.log("BUZZ")}
+// if number is not divisble by 3 or 5 it will be logged to screen.
+  else {
+    console.log(number)
+  }
+}
+// ______________________________________________________________________________
+
+// Challenge 3
+// CHESSBOARD
+
+let grid2 = " # # # #"
+let grid1 = "# # # # "
+let countr = 0
+
+while (countr < 4) {
+  console.log(grid2)
+  console.log(grid1)
+  countr = countr +1
+}
+
 //______________________________________________________________________________
-
-//Challenge 3
-//CHESSBOARD
-//
-// let grid2 = " # # # #"
-// let grid1 = "# # # # "
-// let counter = 0
-//
-// while (counter < 4) {
-//   console.log(grid2)
-//   console.log(grid1)
-//   counter = counter +1
-// }
-
-// //______________________________________________________________________________
 
 // FUNCTIONS
 
-// //Challenge 4
-// // minimum
-//
-// creating my function named min which has 2 parameters.
-// function min (arg1, arg2){
-//   // Math.min takes 2 arguments from the min function and logs the smaller number.
-//   console.log(Math.min(arg1, arg2))
-// }
-// // I call my function with arguments.
-// min(223423,3223432580)
+//Challenge 4
+// minimum
 
-// //______________________________________________________________________________
+// creating my function named min which has 2 parameters.
+function min (arg1, arg2){
+  // Math.min takes 2 arguments from the min function and logs the smaller number.
+  console.log(Math.min(arg1, arg2))
+}
+// I call my function with arguments.
+min(223423,3223432580)
+
+//______________________________________________________________________________
 
 // Challenge 5
-// RECURSION - Is esentially counting down within a function
+// RECURSION - Using a function within a function with an additional variable.
 
-// function isEven(n){
-// //Termination
-//   if (n < 0) return
-// // BASE
-//   if (n === 0) return 1
-// // Recursion
-// x = n * isEven(n -1)
-// return x === 0 ? true : false
-// }
-//
-// console.log(isEven(50))
-// console.log(isEven(75))
-// //______________________________________________________________________________
+function isEven(n){
+//Termination
+  if (n < 0) return
+// BASE
+  if (n === 0) return 1
+// Recursion
+x = n * isEven(n -1)
+return x === 0 ? true : false
+}
 
-//Challenge 6
+console.log(isEven(50))
+console.log(isEven(75))
+//______________________________________________________________________________
+
+// Challenge 6
 // Bean counting
 
-// //The variable for storing the number of letters
-// let b = 0
-//
-// function countBbs(string) {
-//   //Using a for loop with index, will iterate length of string
-//   for (let i = 0; i < string.length; i++) {
-//     //for each index object in string is equal to B the b variable will add 1
-//     if (string[i] == "B") { b++
-//     }
-//   }// returning the final tally of b to be logged to screen
-//   return b
-// }
-//
-// console.log(countBbs("BeansOnBeansOnBeans"))
+//The variable for storing the number of letters
+let b = 0
 
-// // Part 2:
-//
-// let letr = 0
-//
-// function countChar(string, letter) {
-//   for (let i = 0; i < string.length; i++) {
-//     if (string[i] == letter){ letr++
-//     }
-//   }
-//   return letr
-// }
-// console.log(countChar("Bean counter beans all day!", "a"))
+function countBbs(string) {
+  //Using a for loop with index, will iterate length of string
+  for (let i = 0; i < string.length; i++) {
+    //for each index object in string is equal to B the b variable will add 1
+    if (string[i] == "B") { b++
+    }
+  }// returning the final tally of b to be logged to screen
+  return b
+}
+console.log(countBbs("BeansOnBeansOnBeans"))
 
-// //______________________________________________________________________________
+// Part 2:
+
+let letr = 0
+
+function countChar(string, letter) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == letter){ letr++
+    }
+  }
+  return letr
+}
+console.log(countChar("Bean counter beans all day!", "a"))
+
+//______________________________________________________________________________
